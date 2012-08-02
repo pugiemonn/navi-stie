762b100459ae2e2f2240b4a0f75a319bd0fa199a
Pugiemonn <pugiemonn@gmail.com>
2012-08-02 23:55:48 +0900

    navi名が存在するのかチェックするようにした

--- app/Controller/NavisController.php
+++ app/Controller/NavisController.php
@@ -9,6 +9,10 @@ class NavisController extends AppController {
       'order' => 'modified desc',
       'limit' => 20
     );
+    $navi_name = isset($this->request->params['navi_name']) ? $this->request->params['navi_name'] : 0;
+    $navi_param  = $this->check_navi($navi_name);
+    #pr($navi_name);
+
     $title_for_layout = SITE_TITLE;
     #pr($debug);
     $todoufuken  = Configure::read('todoufuken');
@@ -21,13 +25,16 @@ class NavisController extends AppController {
 
   public  function todoufuken($id = null)
   {
+    $navi_name   = isset($this->request->params['navi_name']) ? $this->request->params['navi_name'] : 0;
+    $navi_param  = $this->check_navi($navi_name);
     $todoufuken  = Configure::read('todoufuken');
     $tiiki       = Configure::read('tiiki');
     $tiiki_group = Configure::read('tiiki_group');
+    #$shop_list   = $this->Shop->find('all');
       
     #$this->Post->id = $id;
     #$this->set('post', $this->Post->read());
-    $this->set(compact('title_for_layout', 'tiiki'));
+    $this->set(compact('title_for_layout', 'tiiki', 'navi_param'));
     $this->render();
   }
   
@@ -53,6 +60,28 @@ class NavisController extends AppController {
       }
     }
   } 
+  
+  #naviが存在するかチェックする
+  public function check_navi($navi_name = 0)
+  {
+    $flag = 0;
+    $navi_list = Configure::read('navi_list'); 
+    foreach($navi_list as $item){
+      if($item == $navi_name)
+      {
+        $flag = 1;
+      }
+    }
+    if($flag == 1)
+    {
+      
+      return Configure::read('navi_param.'.$navi_name);
+    }
+    else
+    {
+      //リダイレクト処理
+    }
+  }
 }
 
 ?>
